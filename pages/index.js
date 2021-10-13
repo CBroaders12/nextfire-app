35d894ed63b0a8a5ead9f1bd0ff2554907687785
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-10-13 15:49:27 -0400

    toasting toast

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import toast from 'react-hot-toast';
+
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
+      <button onClick={() => toast.success('hello toast!')}>Toast Me!</button>
     </main>
   );
 }
