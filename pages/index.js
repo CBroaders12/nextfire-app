da27e5c4bef03b6778f8e8d92fbc626b7d834b96
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-10-13 14:15:36 -0400

    adding routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div>
+      <Link
+        prefetch={false}
+        href={{ pathname: '/[username]', query: { username: 'cfreak12' } }}
+      >
+        <a>Conor's Profile</a>
+      </Link>
+    </div>
+  );
 }
