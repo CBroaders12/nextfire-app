7e1a9253ab3ddd2c8521dfa14acfa72ffda1cc1b
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-10-18 20:16:22 -0400

    isr for post page and post content markdown formatting

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,51 @@
-export default function PostPage({}) {
+import PostContent from '../../components/PostContent';
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup('posts').get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    // must be in this format:
+    // paths: [
+    //  { params: { username, slug } }
+    // ]
+
+    paths,
+    fallback: 'blocking',
+  };
+}
+
+export default function PostPage(props) {
   return (
     <main>
-      <h1>Post Page</h1>
+      <PostContent post={props.post} />
     </main>
   );
 }
