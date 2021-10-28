0bd57a560b899c16acc92d4337ec19688d9b29dd
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-10-27 20:18:59 -0400

    heart posts

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,8 +1,13 @@
-import PostContent from '../../components/PostContent';
-import Metatags from '../../components/Metatags';
 import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 import styles from '../../styles/Post.module.css';
+
+import Link from 'next/link';
+
+import PostContent from '../../components/PostContent';
+import Metatags from '../../components/Metatags';
+import AuthCheck from '../../components/AuthCheck';
+import HeartButton from '../../components/HeartButton';
 
 export async function getStaticProps({ params }) {
   const { username, slug } = params;
@@ -63,6 +68,16 @@ export default function Post(props) {
           <p>
             <strong>{post.heartCount || 0} ❤️</strong>
           </p>
+
+          <AuthCheck
+            fallback={
+              <Link href='/enter'>
+                <button>💗 Sign Up</button>
+              </Link>
+            }
+          >
+            <HeartButton postRef={postRef} />
+          </AuthCheck>
         </aside>
       </main>
     </>
